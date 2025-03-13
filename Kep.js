c5dae520da4141026d9a5bc0ffc2ab6e2cd23686
Domawas <dominknagy@gmail.com>
2025-03-13 12:15:35 +0100

    kész

--- Kep.js
+++ Kep.js
@@ -1,12 +1,22 @@
 export default class Kep {
   #eUt;
   #cim; /*privát adattag */
-  constructor(eUt, cim, szulElem) {
+  #index;
+  constructor(eUt, cim, index, szulElem) {
     /*beállítja az adattagokat */
     this.#eUt = eUt;
     this.#cim = cim;
+    this.#index = index;
     this.szulElem = szulElem; /*publikus adattag */
     this.megjelenit();
+    this.kepElem = document.querySelector(".kep:last-child");
+    this.kepElem.addEventListener("click", () => {
+      console.log(this.#index);
+      /*NINCS RETURN, ehelyett létrehozunk egy saját eseményt - neve: kivalaszt. A saját esényunkre tetszőleges osztályban feltudunk iratkozni */
+
+      const e = new CustomEvent("kivalaszt", { detail: this.#index }); /*visszaadom az objektumnak a sorszámát */
+      window.dispatchEvent(e);
+    });
   }
   /*kell-e getter es setter valójában (akarjuk e változtatni a képeket?)
     mivel a galériában a képek elérési utját nem akarjuk változtatni, ezért nem kell */
@@ -16,6 +26,8 @@ export default class Kep {
     let html = `<div class="kep">
         <img src="${this.#eUt}" alt="${this.#cim}">
       </div>`;
-    this.szulElem.innerHTML += html;
+    /* innerHTML -html kodot illesztunk be de nem kapunk html dom objektumot
+    Ezért használjuk insertAdjacentHTML  */
+    this.szulElem.insertAdjacentHTML("beforeend", html);
   }
 }
--- NagyKep.js
+++ NagyKep.js
@@ -1,44 +1,56 @@
-import KiemeltKep from "./KiemeltKep.js"
+import KiemeltKep from "./KiemeltKep.js";
 
-export default class NagyKep{
-    #aktIndex=0;
-    #lista = []
+export default class NagyKep {
+  #aktIndex = 0;
+  #lista = [];
 
-    constructor(lista){
-        this.nagyKepElem = document.getElementsByClassName("kiemeltkep")[0];
-        this.#lista=lista
-        new KiemeltKep(
-            this.#lista[this.#aktIndex].kep, 
-            this.#lista[this.#aktIndex].cim, 
-            this.nagyKepElem);
-        
+  constructor(lista) {
+    this.nagyKepElem = document.getElementsByClassName("kiemeltkep")[0];
+    this.#lista = lista;
+    new KiemeltKep(
+      this.#lista[this.#aktIndex].kep,
+      this.#lista[this.#aktIndex].cim,
+      this.nagyKepElem
+    );
 
-        this.balElem = document.getElementsByClassName("bal")[0];
-        this.jobbElem = document.getElementsByClassName("jobb")[0];
-        this.esemenykezelok()
-    }
+    this.balElem = document.getElementsByClassName("bal")[0];
+    this.jobbElem = document.getElementsByClassName("jobb")[0];
+    this.esemenykezelok();
+  }
 
-    esemenykezelok() {
-        this.balElem.addEventListener("click", () => {
-            this.#aktIndex--;
-            this.#aktIndex < 0 ? (this.#aktIndex = this.#lista.length - 1) : (this.#aktIndex = this.#aktIndex);
-            new KiemeltKep(
-                this.#lista[this.#aktIndex].kep,
-                this.#lista[this.#aktIndex].cim,
-                this.nagyKepElem
-            );
-        });
+  esemenykezelok() {
+    window.addEventListener("kivalaszt", (event) => {  /*Feliratkozunk a saját eseményünkre, Mindíg így kell */
+      console.log(event.detail);
+      this.#aktIndex = event.detail;
+      new KiemeltKep(
+        this.#lista[this.#aktIndex].kep,
+        this.#lista[this.#aktIndex].cim,
+        this.nagyKepElem
+      );
+    });
 
-        this.jobbElem.addEventListener("click", () => {
-            this.#aktIndex++;
-            this.#aktIndex > this.#lista.length - 1 ? (this.#aktIndex = 0) : (this.#aktIndex = this.#aktIndex);
-            new KiemeltKep(
-                this.#lista[this.#aktIndex].kep,
-                this.#lista[this.#aktIndex].cim,
-                this.nagyKepElem
-            );
-        });
-    }
+    this.balElem.addEventListener("click", () => {
+      this.#aktIndex--;
+      this.#aktIndex < 0
+        ? (this.#aktIndex = this.#lista.length - 1)
+        : (this.#aktIndex = this.#aktIndex);
+      new KiemeltKep(
+        this.#lista[this.#aktIndex].kep,
+        this.#lista[this.#aktIndex].cim,
+        this.nagyKepElem
+      );
+    });
 
-
+    this.jobbElem.addEventListener("click", () => {
+      this.#aktIndex++;
+      this.#aktIndex > this.#lista.length - 1
+        ? (this.#aktIndex = 0)
+        : (this.#aktIndex = this.#aktIndex);
+      new KiemeltKep(
+        this.#lista[this.#aktIndex].kep,
+        this.#lista[this.#aktIndex].cim,
+        this.nagyKepElem
+      );
+    });
+  }
 }
