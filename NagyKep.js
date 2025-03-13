96aa0cb87e884cf3ae68d58be46539f240a16e9e
Domawas <dominknagy@gmail.com>
2025-03-13 10:58:29 +0100

    kepgaleria

--- NagyKep.js
+++ NagyKep.js
@@ -1,56 +1,55 @@
 import KiemeltKep from "./KiemeltKep.js";
 
-export default class NagyKep{
-    
-    
-    constructor(lista){
-        this.aktIndex=2
-        this.lista= []
+export default class NagyKep {
+    constructor(lista) {
+        this.aktIndex = 2;  
+        this.lista = lista; 
         this.nagyKepElem = document.getElementsByClassName("kiemeltkep")[0];
-        this.lista=lista
+
+
         new KiemeltKep(
             this.lista[this.aktIndex].kep,
             this.lista[this.aktIndex].cim,
             this.nagyKepElem
         );
-    this.balElem = document.querySelector(".bal");
-    this.jobbElem = document.querySelector(".jobb");
-    this.esemenykezelok()
+
+        this.balElem = document.querySelector(".bal");
+        this.jobbElem = document.querySelector(".jobb");
+
+        this.esemenykezelok();
     }
 
-    esemenykezelok(){
-        this.balElem.addEventListener("click", function () {
+    esemenykezelok() {
+        
+        this.balElem.addEventListener("click", () => { 
             console.log("bal");
-            this.aktIndex--;
-            this.aktIndex < 0 ? ( this.aktIndex  = this.lista.length - 1) : (this.aktIndex = this.aktIndex);
-            const NAGYKEP = new KiemeltKep(
-              this.lista[this.aktIndex].kep,
-              this.lista[this.aktIndex].cim,
-              this.nagyKepElem
-            );
-          });
-          this.jobbElem.addEventListener("click", function () {
-            console.log("jobb");
-            this.aktIndex++;
-            this.aktIndex > this.lista.length - 1 ? (this.aktIndex = 0) : (this.aktIndex = aktIndex);
-            const NAGYKEP = new KiemeltKep(
+            this.aktIndex--;  
+            if (this.aktIndex < 0) {
+                this.aktIndex = this.lista.length - 1; 
+            }
+
+          
+            new KiemeltKep(
                 this.lista[this.aktIndex].kep,
                 this.lista[this.aktIndex].cim,
-              this.nagyKepElem
+                this.nagyKepElem
             );
-          });
+        });
+
+        
+        this.jobbElem.addEventListener("click", () => { 
+            console.log("jobb");
+            this.aktIndex++;  
+            if (this.aktIndex >= this.lista.length) {
+                this.aktIndex = 0;  
+            }
+
+            
+            new KiemeltKep(
+                this.lista[this.aktIndex].kep,
+                this.lista[this.aktIndex].cim,
+                this.nagyKepElem
+            );
+        });
     }
-
 }
-
-
-/* bal és a jobb gombbal léptessük a képeket */
-
-/*1. meg kell fogni a gombokat */
-/*2. hozzáadjuk az eseménykezelőket */
-
-
-
-/*3. A lista következő elemével példányosítjuk a NAGYKEP-et */
-
-
